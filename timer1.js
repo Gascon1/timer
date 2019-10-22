2f4c2455f8e6173e285f35fc1ba0845b344e541a
Mikel Gascon <mikelgascon@outlook.com>
2019-10-22 18:45:16 +0000

    created a function to emit a beep sound at the intervals specified by the user

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const beepCommand = () => {
+  let userInput = (process.argv.slice(2));
+  for (let time of userInput) {
+    console.log(time);
+    if (Number(time) > 0) {
+    setTimeout(() => {process.stdout.write('\x07')}, (time * 1000));
+    }
+  }
+}
+
+beepCommand();
